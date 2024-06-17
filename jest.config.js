4a745e34214b45de16e5807749a349537d7cb60a
NevillerBA <123617103+leticia2983@users.noreply.github.com>
2024-06-17 17:25:56 +0400

    added npm install --save-dev ts-jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  };
-};

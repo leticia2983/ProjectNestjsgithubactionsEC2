e505e7d442424e5d96ffe6ef3eb1f5c7710113e8
NevillerBA <123617103+leticia2983@users.noreply.github.com>
2024-06-17 14:00:42 +0400

    added Nestjs Hello World Application and github/workflows directory

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -1,15 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsController } from './items.controller';
+import { ItemsService } from './items.service';
 
 describe('Items Controller', () => {
   let controller: ItemsController;
+  let service: ItemsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ItemsController],
+      providers: [ItemsService],
     }).compile();
 
     controller = module.get<ItemsController>(ItemsController);
+    service = module.get<ItemsService>(ItemsService);
   });
 
   it('should be defined', () => {
